1bee20b5d3a172b59a6fff18588137645f9920b6
javenzamojcin <javen.william@gmail.com>
2023-11-07 23:42:16 -0500

    impl detectioncomponent

--- src/Components/DetectionComponent/DetectionComponent.gd
+++ src/Components/DetectionComponent/DetectionComponent.gd
@@ -1,8 +1,16 @@
 extends Area2D
 
 @export var radius : float = 750.0
+var body_detected : Node2D
 
 func _ready():
 	# update detection shape radius
 	if self.radius > 0.0:
 		$CollisionShape.shape.radius = self.radius
+
+func _on_detection_area_body_entered(_body):
+	# uses collision layer to detect
+	self.body_detected = _body
+
+func _on_detection_area_body_exited(_body):
+	self.body_detected = null

62e1385ab25910c20812019e4e131a017d1fab44
javenzamojcin <javen.william@gmail.com>
2023-11-08 00:21:41 -0500

    impl mob damage component

---
+++ src/Components/DamageProcessComponents/Mob/MobDamageComponent.gd
@@ -0,0 +1,9 @@
+extends "res://Components/DamageProcessComponents/DamageProcess.gd"
+class_name MobDamageComponent
+
+
+func process_damage() -> float:
+	"""
+	Implementation function for processing mob damage.
+	"""
+	return 0.0

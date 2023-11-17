cc5a47afbe2f7bf4c0c215eb646f87c8a043f32d
Nicole-Schneider <schne542@msu.edu>
2023-11-17 17:50:17 -0500

    Added damage animations

--- README.md
+++
@@ -1,2 +0,0 @@
-# evol-nn-npc
-CSE848 term project: evolutionary neural network for game NPC AI
--- src/Components/TileMapComponent/TileMapComponent.gd
+++
@@ -1,6 +0,0 @@
-extends TileMap
-class_name TileMapComponent
-
-func get_random_tile(layer_id: int):
-	var tile = Util.choose(self.get_used_cells(layer_id))
-	return self.map_to_local(tile)

1ab2684a56eb522dcb1312f2bc6e757e43ab6cc1
javenzamojcin <javen.william@gmail.com>
2023-11-22 13:50:48 -0500

    impl output decision class

---
+++ src/Components/BrainComponent/NN/OutputDecision.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class OutputDecision : Node
+{
+    public int x {set; get;}
+    public int y {set; get;}
+    public bool jump {set; get;}
+    public bool attack {set; get;}
+}

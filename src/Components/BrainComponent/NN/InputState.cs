f9807d767ff614930f0a0be8b59ba967363f5ff3
javenzamojcin <javen.william@gmail.com>
2023-11-27 11:30:29 -0500

    impl inputstate class

--- src/Components/BrainComponent/NN/InputState.cs
+++ src/Components/BrainComponent/NN/InputState.cs
@@ -5,4 +5,17 @@ using System;
 public partial class InputState : Resource
 {
     public float MyHealth {set; get;}
+
+    public float EnemyHealth {set; get;}
+
+    public int MyState {set; get;}
+
+    public int EnemyState {set; get;}
+
+    public float DistanceToEnemy {set; get;}
+
+    public float NextXToEnemy {set; get;}
+
+    public float NextYToEnemy {set; get;}
+
 }

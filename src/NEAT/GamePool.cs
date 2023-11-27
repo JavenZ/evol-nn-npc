b151132c64acdcc52dd1ad6afa677e8f1ad20341
javenzamojcin <javen.william@gmail.com>
2023-11-27 14:47:05 -0500

    add game session logging

--- src/NEAT/GamePool.cs
+++ src/NEAT/GamePool.cs
@@ -28,6 +28,7 @@ public partial class GamePool
         {
             // Instantiate game session
             var session = new GameSession();
+            session.ID = i;
 
             // Instantiate game node
             session.Game = game_scene.Instantiate() as Node2D;
@@ -62,7 +63,7 @@ public partial class GamePool
     public async Task<GameResults> JoinGame(IBlackBox<double> box, String team)
     {
         // THREAD SAFE
-        GD.Print("Joining game...");
+        // GD.Print("Joining game...");
         GameSession session;
 
         // Select first available game from pool & update respective brain components with box
@@ -72,12 +73,14 @@ public partial class GamePool
             session = Pool.First(session => !session.TeamAReady);
             session.TeamABrain.Box = box;
             session.TeamAReady = true;
+            GD.Print($"TeamA joined session {session.ID}");
         }
         else if (team == "TeamB")
         {
             session = Pool.First(session => !session.TeamBReady);
             session.TeamBBrain.Box = box;
             session.TeamBReady = true;
+            GD.Print($"TeamB joined session {session.ID}");
         }
         else
         {
@@ -86,22 +89,27 @@ public partial class GamePool
             session.TeamBBrain.Box = box;
             session.TeamAReady = true;
             session.TeamBReady = true;
+            GD.Print($"TeamA & TeamB joined session {session.ID}");
         }
 
         // Start game session if both teams are ready
         if (session.TeamAReady && session.TeamBReady)
+        {
             Trainer.CallDeferred("add_child", session.Game);
+            GD.Print($"Started session {session.ID}!");
+        }
 
         Mutex.Unlock();
 
         // Wait for game session to finish
         var results = await session.Game.ToSignal(session.Game, "finished");
-        GD.Print("Game finished!");
+        GD.Print($"Session {session.ID} finished!");
 
         // Return game results
         return results[0].As<GameResults>();
     }
     private class GameSession {
+        public int ID {set; get;}
         public Node2D Game {set; get;}
 
         public Array<Node> TeamA = new Array<Node>();

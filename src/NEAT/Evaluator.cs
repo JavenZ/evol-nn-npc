0302c87cf5477e1f270c945df9244f454140002b
javenzamojcin <javen.william@gmail.com>
2023-11-28 23:57:04 -0500

    adjust fitness calculation

--- src/NEAT/Evaluator.cs
+++ src/NEAT/Evaluator.cs
@@ -16,6 +16,7 @@ public sealed class Evaluator : IPhenomeEvaluator<IBlackBox<double>>
 
         // Game time reward
         fitness += (1.0 - results.MatchTime) * 8.0;
+        // if (results.MatchTime < 1.0) fitness += 5.0;
         
         if (results.Winner == "Tie") {
             fitness += 5.0;
@@ -30,12 +31,16 @@ public sealed class Evaluator : IPhenomeEvaluator<IBlackBox<double>>
         }
 
         if (Team == "TeamA") {
-            fitness += Math.Max(results.TeamBDmgReceived - (results.TeamADmgReceived * 1.2), 0.0) * 15.0;
-            fitness += Math.Max(results.TeamBDeaths - results.TeamADeaths, 0.0) * 5.0;
+            // fitness += Math.Max(results.TeamBDmgReceived - (results.TeamADmgReceived * 1.2), 0.0) * 15.0;
+            // fitness += Math.Max(results.TeamBDeaths - results.TeamADeaths, 0.0) * 5.0;
+            fitness += (1.0 - results.TeamADmgReceived) * 10.0; 
+            fitness += results.TeamBDeaths * 7.5;
         }
         else {
-            fitness += Math.Max(results.TeamADmgReceived - (results.TeamBDmgReceived * 1.2), 0.0) * 15.0;
-            fitness += Math.Max(results.TeamADeaths - results.TeamBDeaths, 0.0) * 5.0;
+            // fitness += Math.Max(results.TeamADmgReceived - (results.TeamBDmgReceived * 1.2), 0.0) * 15.0;
+            // fitness += Math.Max(results.TeamADeaths - results.TeamBDeaths, 0.0) * 5.0;
+            fitness += (1.0 - results.TeamBDmgReceived) * 10.0; 
+            fitness += results.TeamADeaths * 7.5;
         }
 
         GD.Print(results + $", {Team}_Fit={fitness}");

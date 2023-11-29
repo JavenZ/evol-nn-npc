c84212319516f35368a86b21373e83dfe6ca2300
javenzamojcin <javen.william@gmail.com>
2023-11-29 01:23:53 -0500

    adjust fitness calculation

--- src/NEAT/Evaluator.cs
+++ src/NEAT/Evaluator.cs
@@ -9,40 +9,34 @@ public sealed class Evaluator : IPhenomeEvaluator<IBlackBox<double>>
 
     public String Team {set; get;}
 
+    const double WIN_REWARD = 10.0;
+    const double TIME_REWARD = 8.0;
+    const double HEALTH_REWARD = 7.5;
+    const double DMG_REWARD = 7.5;
+    const double KILL_REWARD = 5.0;
+
     public async Task<FitnessInfo> Evaluate(IBlackBox<double> box)
     {
         var results = await GamePool.JoinGame(box, Team);
         double fitness = 0.0;
 
         // Game time reward
-        fitness += (1.0 - results.MatchTime) * 8.0;
-        // if (results.MatchTime < 1.0) fitness += 5.0;
+        fitness += (1.0 - results.MatchTime) * TIME_REWARD;
         
-        if (results.Winner == "Tie") {
-            fitness += 5.0;
-        }
         // Winner is current team
-        else if (results.Winner == Team) {
-            fitness += 10.0;
-        }
-        // Winner is other team
-        else {
-            fitness += 0.0;
+        if (results.Winner == Team) {
+            fitness += WIN_REWARD;
         }
 
         if (Team == "TeamA") {
-            // fitness += Math.Max(results.TeamBDmgReceived - (results.TeamADmgReceived * 1.2), 0.0) * 15.0;
-            // fitness += Math.Max(results.TeamBDeaths - results.TeamADeaths, 0.0) * 5.0;
-            fitness += (1.0 - results.TeamADmgReceived) * 7.5;
-            fitness += results.TeamBDmgReceived * 7.5;
-            fitness += results.TeamBDeaths * 5.0;
+            fitness += (1.0 - results.TeamADmgReceived) * HEALTH_REWARD;
+            fitness += results.TeamBDmgReceived * DMG_REWARD;
+            fitness += results.TeamBDeaths * KILL_REWARD;
         }
-        else {
-            // fitness += Math.Max(results.TeamADmgReceived - (results.TeamBDmgReceived * 1.2), 0.0) * 15.0;
-            // fitness += Math.Max(results.TeamADeaths - results.TeamBDeaths, 0.0) * 5.0;
-            fitness += (1.0 - results.TeamBDmgReceived) * 7.5;
-            fitness += results.TeamADmgReceived * 7.5;
-            fitness += results.TeamADeaths * 5.0;
+        else if (Team == "TeamB") {
+            fitness += (1.0 - results.TeamBDmgReceived) * HEALTH_REWARD;
+            fitness += results.TeamADmgReceived * DMG_REWARD;
+            fitness += results.TeamADeaths * KILL_REWARD;
         }
 
         GD.Print(results + $", {Team}_Fit={fitness}");
